95b7259bd417e3e90c2ad62243a4c7b32190cdc2
camiboj <camiboj@gmail.com>
2019-07-31 17:31:16 -0300

    ls0 done

--- ls0.c
+++ ls0.c
@@ -2,14 +2,33 @@
 // Created by camix on 31/7/19.
 //
 #define _POSIX_C_SOURCE 200809L
+#define LS0_OPENDIR_FAIL 3
+#define LS0_CLOSEDIR_FAIL 4
+#define MAX_DIR_NAME 1024
+
 
 #include <unistd.h>
-
+#include <sys/types.h>
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int ls0(const char *file) {
-    
+
+int ls0(const char *dir_name) {
+    DIR* dirp = opendir(dir_name);
+    if (dirp == NULL) {
+        return LS0_OPENDIR_FAIL;
+    }
+    struct dirent *res;
+    while((res = readdir(dirp)) != NULL) {
+        fprintf(stdout, "%s\t", res->d_name);
+    }
+    int s = closedir(dirp);
+    if (s == -1) {
+        return LS0_CLOSEDIR_FAIL;
+    }
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -19,7 +38,7 @@ int main(int argc, char *argv[]) {
     }
     if (argc == 2) {
         return ls0(argv[1]);
-    } else {
-        return ls0(NULL);
     }
+    char dir_name[MAX_DIR_NAME];
+    return ls0(getcwd(dir_name, MAX_DIR_NAME));
 }
